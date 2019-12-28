ce9fdd9d32ba46ba80439dbfe248e7202a3575e1
Nobile_Diniz <gabrielnd@gmail.com>
2019-12-28 16:19:35 -0300

    176.Using virtual and override How to make a function of a parent class make appearance in a child class

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "TankPlayerController.h"
 #include "Tank.h"
 
 // Sets default values
@@ -15,7 +15,8 @@ ATank::ATank()
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+
 }
 
 // Called every frame
